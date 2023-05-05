b2aceb642ac01f044b9604f9e46fd14bc4082f71
emmtell-dux <duxemm@gmail.com>
2023-05-05 13:13:16 -0500

    footer, navbar responsive

--- src/app/components/nav-var/nav-var/nav-var.component.ts
+++ src/app/components/nav-var/nav-var/nav-var.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav-var',
   templateUrl: './nav-var.component.html',
-  styleUrls: ['./nav-var.component.scss']
+  styleUrls: ['./nav-var.component.scss'],
 })
 export class NavVarComponent {
+  mostrarMenu = '';
 
+  showMenu(clase: string) {
+    switch (clase) {
+      case 'menu-list':
+        this.mostrarMenu = 'menu-list-no';
+        break;
+      case 'menu-list-no':
+        this.mostrarMenu = 'menu-list';
+        break;
+      default:
+        this.mostrarMenu = 'menu-list'
+        break;
+    }
+  }
+
+  hideMenu() {
+    this.mostrarMenu = 'menu-list-no';
+  }
 }
